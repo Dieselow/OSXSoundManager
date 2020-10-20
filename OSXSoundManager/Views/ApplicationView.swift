f571afeec291d56aa5d98444f6ccd906350328d6
Louis Dumont <louisdumontbarbosa@gmail.com>
2020-10-20 18:53:53 +0200

    Algin images and text on each lines

--- OSXSoundManager/Views/ApplicationView.swift
+++ OSXSoundManager/Views/ApplicationView.swift
@@ -11,15 +11,15 @@ struct ApplicationView: View {
     var application: NSRunningApplication;
     
     var body: some View {
-        return ZStack {
+        return HStack {
             Image(nsImage: NSWorkspace.shared.icon(forFile: application.bundleURL?.path ?? "Default value"))
                 .resizable()
                 .aspectRatio(contentMode: .fill)
                 .frame(width: 22, height: 22)
                 .clipped()
                 .cornerRadius(20.0)
-            Spacer()
             Text("\(self.application.localizedName ?? "Couldn't retrieve name")");
+            Spacer()
         }
     }
 }
